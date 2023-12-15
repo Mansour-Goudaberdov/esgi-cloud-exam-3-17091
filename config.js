347db1e572a0be8e58745558f7d97f30a04676a2
Mans <mansourgoudaberdov@gmail.com>
2023-12-15 11:13:17 +0100

    Initial Commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'ZCSBI6P2OH79S9VFQKL4', // TODO
+    secretAccessKey: 'WiwnwdTWyFDl5JgMDkvgf6nWQn7BwJEj3WjA3Yn5', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-17091', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
